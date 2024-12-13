d2cab077f72b325351f4b31cf5c231db64fde3ef
Александр Горохов <39surara@gmail.com>
2024-12-13 22:56:46 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.GorohovAI.Sprint3.Task0.V21.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GorohovAI.Sprint3.Task0.V21.Lib
+{
+    public class DataService : ISprint3Task0V21
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double S =0;
+            for (int k = startValue; k <= stopValue; k++)
+            {
+                S += (Math.Pow(value, k) + 1.0 / 4) * Math.Sin(k);
+            }
+
+            // Вывод результат
+            return S;
+        }
+    }
+}
