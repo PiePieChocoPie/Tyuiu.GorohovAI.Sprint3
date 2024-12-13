534ee2b5fd14d8d4cf4fa03cf4ab670003127ba4
Александр Горохов <39surara@gmail.com>
2024-12-13 23:16:15 +0500

    312321

---
+++ Tyuiu.GorohovAI.Sprint3.Task2.V12.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GorohovAI.Sprint3.Task2.V12.Lib
+{
+    public class DataService : ISprint3Task2V12
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double P = 0;
+            do
+            {
+                P *= Math.Pow(300.0 / (startValue + Math.Pow(value, startValue)), startValue);
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(P,3);
+        }
+    }
+}
